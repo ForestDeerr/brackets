4646185397742b52687fa02a4c7d590dd44d7856
EgorGer <QwertiQQQ@gmal.com>
2024-10-09 14:03:51 +0300

    init: logic function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const openingBrackets = bracketsConfig.map(pair => pair[0]);
+  const closingBrackets = bracketsConfig.map(pair => pair[1]);
+  const matchingBrackets = Object.fromEntries(bracketsConfig);
+
+  for (let bracket of str) {
+      if (openingBrackets.includes(bracket)) {
+          if (closingBrackets.includes(bracket) && stack.length > 0 && stack[stack.length - 1] === bracket) {
+              stack.pop();
+          } else {
+              stack.push(bracket);
+          }
+      } else {
+          if (stack.length === 0 || matchingBrackets[stack.pop()] !== bracket) {
+              return false;
+          }
+      }
+  }
+
+  return stack.length === 0;
 }
